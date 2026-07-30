fc13df51dbf950bc7d798e25887ecbc29ab40203
Monet <codem0n3t@gmail.com>
2026-07-29 23:50:13 -0400

    Header (nav, logo, and log in/sign up) set up

--- locshop-client/src/component/Header/Header.jsx
+++ locshop-client/src/component/Header/Header.jsx
@@ -1,16 +1,28 @@
+import React from "react";
 import "./Header.css";
 import logo from "../src/assets/logo";
 
-function Header({}) {
+function Header({ nav, logo }) {
   return (
     <header className="header">
       {/* Makes the logo a clickable link */}
       <Link to="/">
         <img className="header__logo" src={logo} alt="LocShop logo" />
       </Link>
-      <div className="header_nav">
-        <button>onClick = {handleAddClick}</button>
-      </div>
+      <nav className="header_nav">
+        <div>
+          <a href="/home">Home</a>
+          <a href="/services">Services</a>
+          <a href="/gallery">Gallery</a>
+          <a href="/inspiration">Inspiration</a>
+          <a href="/appointment">Appointment</a>
+          <a href="/contact">Contact</a>
+        </div>
+        <div>
+          <button className="header_login">Log In</button>
+          <button className="header_signup">Sign Up</button>
+        </div>
+      </nav>
     </header>
   );
 }
