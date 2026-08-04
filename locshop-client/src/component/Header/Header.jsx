04cbe2af4836cdc07bcb4e56ceeeb0a563e72e72
Monet <codem0n3t@gmail.com>
2026-08-03 23:21:27 -0400

    Header and Home

--- locshop-client/src/component/Header/Header.jsx
+++ locshop-client/src/component/Header/Header.jsx
@@ -1,28 +1,46 @@
 import React from "react";
 import "./Header.css";
-//import logo from "../src/assets/logo";
 
-function Header({ nav, logo }) {
+import { NavLink } from "react-router";
+
+import logo from "../assets/logo.png";
+
+function Header() {
   return (
     <header className="header">
       {/* Makes the logo a clickable link */}
-      <Link to="/home">
-        <img className="header__logo" src={logo} alt="LocShop logo" />
-      </Link>
-      <nav className="header_nav">
+      <nav className="header__nav">
+        <NavLink to="/home" element={<Home />}>
+          <img className="header__logo" src={logo} alt="LocShop logo" />
+        </NavLink>
+
         <div>
-          <a href="/home">Home</a>
-          <a href="/services">Services</a>
-          <a href="/gallery">Gallery</a>
-          <a href="/inspiration">Inspiration</a>
-          <a href="/appointment">Appointment</a>
-          <a href="/contact">Contact</a>
+          <NavLink to="/home" element={<Home />}>
+            Home
+          </NavLink>
+          <NavLink to="/services" element={<Services />}>
+            Services
+          </NavLink>
+          <NavLink to="/gallery" element={<Gallery />}>
+            Gallery
+          </NavLink>
+          <NavLink to="/inspiration" element={<Inspiration />}>
+            Inspiration
+          </NavLink>
+          <NavLink to="/appointment" element={<Appointment />}>
+            Appointment
+          </NavLink>
+          <NavLink to="/contact" element={<Contact />}>
+            Contact
+          </NavLink>
         </div>
         <div>
-          <button className="header_login">Log In</button>
-          <button className="header_signup">Sign Up</button>
+          <button className="header__login">Log In</button>
+          <button className="header__signup">Sign Up</button>
         </div>
       </nav>
     </header>
   );
 }
+
+export default Header;
